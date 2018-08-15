753a9a0a2da666adf18646e2d08508e114fa67be
osmar <oleyet84@gmail.com>
2018-08-15 18:45:03 -0500

    Listo -- TBKT-1AP - Ventas

---
+++ addons/bkt_sales/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Sales Ordeder Ext',
+    'version': '1.0',
+    'category': 'Sales',
+    'sequence': 35,
+    'summary': 'Sales Order Ext',
+    'description': """
+Accounting Access Rights
+========================
+It gives the Administrator user access to all accounting features such as journal items and the chart of accounts.
+
+It assigns manager and user access rights to the Administrator for the accounting application and only user rights to the Demo user.
+""",
+    'website': 'https://www.odoo.com/page/accounting',
+    'depends': ['sale'],
+    'data': [ 
+            'data/ir_sequence_data.xml',
+            'views/sales_ext_views.xml',
+            ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    
+}
---
+++ addons/bkt_sales/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import sales_ext
+from . import project_ext  
+from . import mrp_production_ext
