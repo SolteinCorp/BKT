2dae2b4def7ab3eb99b461959102bd6b0945c285
osmar <oleyet84@gmail.com>
2018-08-16 15:49:30 -0500

    TBKT-3AP - Logística

---
+++ addons/bkt_otp/models/mrp_otp.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models, api, _
+
+class  Picking(models.Model):
+    _inherit = 'stock.picking'
+
+    is_transportation = fields.Boolean('Es tranportación', help='Marcar si la orden es de transportación')
+
+    transportation_type = fields.Selection([
+        ('external', 'Externa'),
+        ('internal', 'Interna')], string='Tipo de transportacion', default='internal')
+
+    transportista_id = fields.Many2one('hr.employee', 'Transportista')
+
+    sale_order_id = fields.Many2one('sale.order', string='Orden de venta asociada', index=True)
+    purchase_order_id = fields.Many2one('purchase.order', string='Orden de compra asociada', index=True, )
+
+    @api.model
+    def create(self, vals):
+        id_pikin = super(Picking,self).create(vals)
+        if 'is_transportation':
+            name = self.env['ir.sequence'].with_context(force_company=vals['company_id']).next_by_code('otp.order')
+
+        self.env['stock.picking'].browse(id_pikin.id).write({'name': name})
+        return id_pikin
+
+
